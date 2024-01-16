e2441a9b925189aa4ac0827446528de2d018c6c7
Craig Marais <craig.marais.85@gmail.com>
2024-01-16 12:17:23 +0100

    Updated Program.cs

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
+        // This is goign to be the best program ever!
         Console.WriteLine("Hello, World!");
+
+        Console.WriteLine(" This is a test.");
     }
 }
